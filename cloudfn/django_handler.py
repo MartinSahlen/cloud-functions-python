546b4df32e49f2ca2ddf48654a30572c55a0f460
Martin Sahlen <martin8900@gmail.com>
2017-05-27 21:54:42 -0400

    Carve out last pieces of work

--- cloudfn/django_handler.py
+++ cloudfn/django_handler.py
@@ -7,5 +7,14 @@ from django.core.handlers.wsgi import WSGIRequest
 def handle_http_event(app):
     environ = wsgi(json.loads(sys.stdin.read()))
     app.load_middleware()
-    res = app.get_response(WSGIRequest(environ))
-    
+    resp = app.get_response(WSGIRequest(environ))
+
+    # handle streaming vs non streaming
+    if resp.streaming:
+        pass
+
+    sys.stdout.write(json.dumps({
+        'body': 'body',
+        'status_code': resp.status_code,
+        'headers': {},
+    }))
