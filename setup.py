f2c88c070cc8775702efb2d3de14db7433234dc6
Martin Sahlen <martin8900@gmail.com>
2017-05-29 16:19:20 -0400

    Update ex

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pycloudfn',
-    version='0.1.187a',
+    version='0.1.188',
     description='GCP Cloud functions in python',
     url='https://github.com/MartinSahlen/cloud-functions-python',
     author='Martin Sahlen',
