1172affc37d609489623432a66d90cdbeea8aeb2
Martin Sahlen <martin8900@gmail.com>
2017-05-29 08:41:01 -0400

    Add unbuffered object for py3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pycloudfn',
-    version='0.1.147',
+    version='0.1.148',
     description='GCP Cloud functions in python',
     url='https://github.com/MartinSahlen/cloud-functions-python',
     author='Martin Sahlen',
