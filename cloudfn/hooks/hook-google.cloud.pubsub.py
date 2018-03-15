29ca12ae7bb483b39dff4ba7b29066a711aab437
seijik42 <seijik42@gmail.com>
2018-03-15 21:50:00 +0900

    added google-api-core in hooks

--- cloudfn/hooks/hook-google.cloud.pubsub.py
+++ cloudfn/hooks/hook-google.cloud.pubsub.py
@@ -2,3 +2,4 @@ from PyInstaller.utils.hooks import copy_metadata
 
 datas = copy_metadata('google-cloud-pubsub')
 datas += copy_metadata('google-cloud-core')
+datas += copy_metadata('google-api-core')
